d01eeecacd11cb0590cda4c2f9b29e07f8e6b316
Pavel Chuvak <chuvak.pavel@gmail.com>
2020-09-18 17:44:03 +0300

    Add ZendeskFramework for Xamarin.Zendesk.iOS

--- Xamarin.Zendesk.iOS/ApiDefinition.cs
+++ Xamarin.Zendesk.iOS/ApiDefinition.cs
@@ -1,12 +1,17 @@
 ﻿using System;
-using CoreGraphics;
-using ObjCRuntime;
+using CoreFoundation;
 using Foundation;
-//using SupportSDK;
-using UIKit;
+using ObjCRuntime;
+using Security;
 
 namespace NativeLibrary
 {
-    
+	// @interface ZendeskFramework : NSObject
+	[BaseType(typeof(NSObject))]
+	interface ZendeskFramework
+	{
+		// -(void)initZendeskWithAppId:(NSString * _Nonnull)appId clientId:(NSString * _Nonnull)clientId zendeskUrl:(NSString * _Nonnull)zendeskUrl __attribute__((objc_method_family("none")));
+		[Export("initZendeskWithAppId:clientId:zendeskUrl:")]
+		void InitZendeskWithAppId(string appId, string clientId, string zendeskUrl);
+	}
 }
-
