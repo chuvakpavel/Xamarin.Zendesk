ef2aa71b061ef432855c1bbbeeddae662acee45b
Pavel Chuvak <chuvak.pavel@gmail.com>
2020-09-22 13:51:01 +0300

    Update Xamarin.Zendesk.iOS

--- Xamarin.Zendesk.iOS/ApiDefinition.cs
+++ Xamarin.Zendesk.iOS/ApiDefinition.cs
@@ -4,7 +4,7 @@ using Foundation;
 using ObjCRuntime;
 using Security;
 
-namespace NativeLibrary
+namespace Xamarin.Zendesk.iOS
 {
 	// @interface ZendeskFramework : NSObject
 	[BaseType(typeof(NSObject))]
@@ -13,5 +13,21 @@ namespace NativeLibrary
 		// -(void)initZendeskWithAppId:(NSString * _Nonnull)appId clientId:(NSString * _Nonnull)clientId zendeskUrl:(NSString * _Nonnull)zendeskUrl __attribute__((objc_method_family("none")));
 		[Export("initZendeskWithAppId:clientId:zendeskUrl:")]
 		void InitZendeskWithAppId(string appId, string clientId, string zendeskUrl);
+
+		// -(void)setAnonymousIdentity;
+		[Export("setAnonymousIdentity")]
+		void SetAnonymousIdentity();
+
+		// -(void)setIdentityWithName:(NSString * _Nonnull)name email:(NSString * _Nonnull)email;
+		[Export("setIdentityWithName:email:")]
+		void SetIdentityWithName(string name, string email);
+
+		// -(void)setUniqueIdentityWithToken:(NSString * _Nonnull)token;
+		[Export("setUniqueIdentityWithToken:")]
+		void SetUniqueIdentityWithToken(string token);
+
+		// -(void)showTicketScreenWithAnimated:(BOOL)animated;
+		[Export("showTicketScreenWithAnimated:")]
+		void ShowTicketScreenWithAnimated(bool animated);
 	}
 }
