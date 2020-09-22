e5c1930e9228860a4cf0950905d1ca211a624518
Pavel Chuvak <chuvak.pavel@gmail.com>
2020-09-22 11:24:56 +0300

    Zendesk.Android binding

---
+++ Xamarin.Zendesk.Droid/ViewAlmostRealProgressBar.cs
@@ -0,0 +1,13 @@
+﻿namespace Zendesk.Support.Request
+{
+    public partial class ViewAlmostRealProgressBar : global::Android.Widget.ProgressBar
+    {
+        public partial class Step : global::Java.Lang.Object, global::Android.OS.IParcelable, global::Java.Lang.IComparable
+        {
+            public int CompareTo(Java.Lang.Object o)
+            {
+                return 0;
+            }
+        }
+    }
+}
