32e59e00eed09ac04fbc735117b135644b3b0308
Pavel Chuvak <chuvak.pavel@gmail.com>
2020-09-29 13:52:17 +0300

    change naming for wrap, support and core

--- Com.Zendesk.Support/Properties/AssemblyInfo.cs
+++ Com.Zendesk.Support/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using Android.App;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Xamarin.ZendeskSupportProvider.Froid")]
+[assembly: AssemblyTitle("Com.Zendesk.Support")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Xamarin.ZendeskSupportProvider.Froid")]
+[assembly: AssemblyProduct("Com.Zendesk.Support")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
